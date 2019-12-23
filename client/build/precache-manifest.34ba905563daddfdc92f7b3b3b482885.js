a25ad180dd99f1cc09761adf8d4ea103904c5763
Vitaliy_B <vlvlv.vovov@bk.ru>
2019-12-23 12:38:33 +0500

    asdc

---
+++ client/build/precache-manifest.34ba905563daddfdc92f7b3b3b482885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462cf5e1524b73a3a09255916e63982d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "647a5aaa9419ddd2e9e9",
+    "url": "/static/css/main.1e9676d0.chunk.css"
+  },
+  {
+    "revision": "c028a3b11a8a724d0865",
+    "url": "/static/js/2.2f01de45.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/static/js/2.2f01de45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "647a5aaa9419ddd2e9e9",
+    "url": "/static/js/main.01ca486f.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
